0d5b45289a7148c7f7be1ce3ddcd154526e36187
Malandru <magnus.leira6@gmail.com>
2025-01-15 14:34:16 +0100

    added base models for entityframework

---
+++ webapi/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace webapi.Models
+{
+    public class Comment
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+
+        public User User { get; set; }
+        public Post ParentPost { get; set; }
+    }
+}
