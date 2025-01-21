945b81b52c94c09de84041198b8085d74671e27a
benedicte vikvam <benedictevikvam@gmail.com>
2025-01-21 11:13:28 +0100

    Create header with nav

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,12 @@
-import Post from "@/components/Post"
+"use client"
+
+import { Header } from "@/components/Header"
 
 export default function Home() {
     return (
-        <Post
-            name="Ola Nordmann"
-            title="Dette er et innlegg"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                    eget risus nec nisl ultricies lacinia. Nullam nec nisl
-                    ultricies, ultricies diam id, aliquam risus. Nulla facilisi.
-                    Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla"
-            profilePicture="https://avatar.iran.liara.run/public/32"
-        />
+        <>
+            <Header />
+            <h1>Hi</h1>
+        </>
     )
 }
