d1ad6f3ceceb778906c4e53c47f67acc22937cfc
benedicte vikvam <benedictevikvam@gmail.com>
2025-01-27 21:23:22 +0100

    Add neighborhood title to header

--- frontend/src/app/components/HeaderNav.tsx
+++ frontend/src/app/components/HeaderNav.tsx
@@ -43,7 +43,7 @@ export const HeaderNav: React.FC<HeaderNavProps> = () => {
                         ))}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Neighborhood</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
