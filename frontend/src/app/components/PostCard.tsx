1e386fa12fe4e64fbbe0f40011462707ae25f580
benedicte vikvam <benedictevikvam@gmail.com>
2025-01-28 14:04:24 +0100

    Add Carousel to PostCard

--- frontend/src/app/components/PostCard.tsx
+++ frontend/src/app/components/PostCard.tsx
@@ -1,24 +1,29 @@
+import { Carousel } from "./Carousel"
+
 type PostCardProps = {
     id?: number
     title: string
     description: string
-    image?: string[]
+    images?: string[]
 }
 
 export const PostCard: React.FC<PostCardProps> = ({
     id,
     title,
     description,
-    image,
+    images,
 }) => {
     return (
         <div className="card bg-base-100 w-96 shadow-sm">
             <figure>
-                {/* Add carousel here */}
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                    alt="Shoes"
-                />
+                {images && images.length > 0 ? (
+                    <Carousel images={images} postCardId={id} />
+                ) : (
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes"
+                    />
+                )}
             </figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
--- frontend/src/app/components/PostCardGrid.tsx
+++ frontend/src/app/components/PostCardGrid.tsx
@@ -6,23 +6,38 @@ export const PostCardGrid: React.FC = () => {
             id: 1,
             title: "Post Title 1",
             description: "Post Description 1",
+            images: [
+                "https://placecats.com/600/400",
+                "https://placecats.com/neo/600/400",
+                "https://placecats.com/g/600/400",
+            ],
         },
         {
             id: 2,
             title: "Post Title 2",
             description: "Post Description 2",
+            images: [
+                "https://placecats.com/millie/600/400",
+                "https://placecats.com/millie_neo/600/400",
+            ],
         },
         {
             id: 3,
             title: "Post Title 3",
             description: "Post Description 3",
+            images: ["https://placecats.com/neo_2/600/400"],
         },
     ]
 
     return (
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
             {posts.map((post) => (
-                <PostCard title={post.title} description={post.description} />
+                <PostCard
+                    title={post.title}
+                    description={post.description}
+                    key={post.title}
+                    images={post.images}
+                />
             ))}
         </div>
     )
