be8e0af2a73d6fb24b73e57caee778eb584bbb63
benedicte vikvam <benedictevikvam@gmail.com>
2025-02-20 14:46:01 +0100

    Edit PostCard to match the new response

--- frontend/src/app/(home)/components/Feed.tsx
+++ frontend/src/app/(home)/components/Feed.tsx
@@ -1,47 +1,51 @@
 "use client"
 
-import { Post } from "@/Models/Post"
 import { PostCard } from "./index"
 import { useUser } from "@/lib/getUser"
+import { useGetPosts } from "../queries"
 
 export const Feed: React.FC = () => {
-    const fakePost: Post = {
-        id: "101",
-        author: {
-            id: "27",
-            name: "Marie Nilsen",
-            avatar: "https://api.dicebear.com/9.x/notionists/svg?seed=Aiden",
-        },
-        title: "Trenger hjelp med snømåking ❄️",
-        content:
-            "Hei naboer! Jeg har skadet foten og sliter med å måke innkjørselen min. Er det noen som har tid til å hjelpe meg i dag? Jeg stiller med kaffe og varm kakao! ☕️❄️",
-        imageList: [
-            "https://plus.unsplash.com/premium_photo-1737912828325-f72452894789?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        ],
-        likes: ["3", "8", "19"],
-        
-        datePosted: new Date("2025-02-03T13:00:00Z"),
-        
-        categoryId: "",
-    }
-
     const user = useUser()
 
-    console.log(user)
+    const { data: posts, isLoading, isError } = useGetPosts(user?.accessToken)
+
+    console.log(posts)
+
+    console.log({ posts, isLoading, isError })
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+
+    if (isError) {
+        return <p>Error</p>
+    }
+
+    if (!posts?.length) {
+        return <p>No posts found</p>
+    }
 
     return (
-        <div className="mx-auto w-full lg:w-96">
-            <PostCard
-                author={fakePost.author}
-                likes={fakePost.likes}
-                title={fakePost.title}
-                content={fakePost.content}
-                key={fakePost.title}
-                imageList={fakePost.imageList}
-                datePosted={fakePost.datePosted}
-                categoryId={fakePost.categoryId}
-                id={fakePost.id}
-            />
+        <div className="mx-auto w-full lg:w-96 xl:w-1/2">
+            {posts.map((post) => (
+                <PostCard
+                    key={post.id}
+                    id={post.id}
+                    title={post.title}
+                    description={post.description}
+                    datePosted={post.datePosted}
+                    dateLastEdited={post.dateLastEdited}
+                    authorUserId={post.authorUserId}
+                    categoryId={post.categoryId}
+                    imageUrls={post.imageUrls}
+                    authorUser={post.authorUser}
+                    commentCount={post.commentCount}
+                    likedByUserCount={post.likedByUserCount}
+                    likedByCurrentUser={post.likedByCurrentUser}
+                />
+            ))}
         </div>
     )
 }
+
+export default Feed
--- frontend/src/app/(home)/components/PostCard.tsx
+++ frontend/src/app/(home)/components/PostCard.tsx
@@ -1,21 +1,26 @@
+import { formatDate } from "@/lib/formatDate"
 import { Carousel } from "./index"
 import { Post } from "@/Models/Post"
 
 export const PostCard: React.FC<Post> = ({
-    id,
     title,
-    author,
-    content,
-    imageList,
-    likes,
-    datePosted,
+    description,
+    imageUrls,
     categoryId,
+    authorUser,
+    commentCount,
+    likedByUserCount,
+    likedByCurrentUser,
+    datePosted,
+    dateLastEdited,
 }) => {
+    const { name, avatar } = authorUser
+
     return (
         <div className="card shadow-sm">
             <figure>
-                {imageList && imageList.length > 0 ? (
-                    <Carousel imageList={imageList} />
+                {imageUrls && imageUrls.length > 0 ? (
+                    <Carousel imageList={imageUrls} />
                 ) : null}
             </figure>
             <div className="card-body">
@@ -23,15 +28,59 @@ export const PostCard: React.FC<Post> = ({
                     <div className="avatar w-8">
                         <div className="mask mask-squircle">
                             <img
+                                alt={name}
                                 className="h-full w-full"
-                                src={author.avatar}
+                                src={avatar}
                             />
                         </div>
                     </div>
-                    <p className="w-1/2 truncate text-sm">{author.name}</p>
+                    <p className="w-1/2 truncate text-sm">{name}</p>
+                    <div className="badge badge-soft badge-primary">
+                        Category
+                    </div>
                 </div>
+                {/* 
+                TODO: Get the right category */}
+                <p className="text-neutral-500">{formatDate(datePosted)}</p>
                 <h2 className="card-title">{title}</h2>
-                <p>{content}</p>
+                <p>{description}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-ghost btn-sm">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="2.5"
+                            stroke="currentColor"
+                            className="size-[1.2em]"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                            />
+                        </svg>
+                        <span>{likedByUserCount}</span>
+                    </button>
+                    <button className="btn btn-ghost btn-sm">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-[1.2em]"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                            />
+                        </svg>
+
+                        <span>{commentCount}</span>
+                    </button>
+                </div>
             </div>
         </div>
     )
