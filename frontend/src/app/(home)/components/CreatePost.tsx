249cdbaaa11d18cea0067b55e705d0268d41fe7e
benedicte vikvam <benedictevikvam@gmail.com>
2025-02-21 16:02:19 +0100

    Clean up code,

--- frontend/src/app/(home)/components/CreatePost.tsx
+++ frontend/src/app/(home)/components/CreatePost.tsx
@@ -1,17 +1,27 @@
 "use client"
 
 import { Category } from "@/Models/Category"
+import { User } from "@/Models/User"
 import { useState } from "react"
+import { Post } from "@/Models/Post"
 
 type CreatePostProps = {
     categories?: Category[]
+    user: User
+    onPostCreated?: (newPost: Post) => void
 }
 
-export const CreatePost: React.FC<CreatePostProps> = ({ categories }) => {
-    const [selectedCategory, setSelectedCategory] = useState<Category>()
+export const CreatePost: React.FC<CreatePostProps> = ({ categories, user }) => {
+    const [selectedCategory, setSelectedCategory] = useState<Category | null>(
+        null
+    )
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
 
     const handleCategorySelect = (category: Category) => {
-        setSelectedCategory(category)
+        setSelectedCategory((prev) =>
+            prev?.id === category.id ? null : category
+        )
     }
 
     return (
@@ -33,42 +43,53 @@ export const CreatePost: React.FC<CreatePostProps> = ({ categories }) => {
                                     strokeLinejoin="round"
                                     d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
                                 ></path>
-                            </svg>{" "}
+                            </svg>
                             Write a new post
                         </div>
                     </div>
                 </div>
-            </div>{" "}
-            <div className="card-body gap-4">
-                <div className="flex items-center justify-between">
-                    <input
-                        type="text"
-                        placeholder="Title"
-                        className="input input-md"
-                    />
-                    <button className="btn btn-xs">Add files</button>
-                </div>
-                <textarea
-                    className="textarea textarea-border w-full resize-none"
-                    placeholder="What's happening?"
-                ></textarea>
-                <div className="flex gap-4">
-                    {categories?.map((category) => {
-                        return (
+            </div>
+            <form>
+                <div className="card-body gap-4">
+                    <div className="flex items-center justify-between">
+                        <input
+                            type="text"
+                            placeholder="Title"
+                            className="input input-md"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
+                        <button className="btn btn-xs">Add files</button>
+                    </div>
+                    <textarea
+                        className="textarea textarea-border w-full resize-none"
+                        placeholder="What's happening?"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                    ></textarea>
+                    <div className="flex flex-wrap gap-4">
+                        {categories?.map((category) => (
                             <button
-                                className={`btn btn-xs btn-${category.color} ${selectedCategory === category ? "btn-active" : "btn-soft"}`}
+                                type="button"
+                                className={`btn btn-xs btn-${category.color} ${
+                                    selectedCategory?.id === category.id
+                                        ? "btn-active"
+                                        : "btn-soft"
+                                }`}
                                 onClick={() => handleCategorySelect(category)}
                                 key={category.id}
                             >
                                 {category.name}
                             </button>
-                        )
-                    })}
+                        ))}
+                    </div>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary" type="submit">
+                            Publish
+                        </button>
+                    </div>
                 </div>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Publish</button>
-                </div>
-            </div>
+            </form>
         </div>
     )
 }
--- frontend/src/app/(home)/queries.ts
+++ frontend/src/app/(home)/queries.ts
@@ -1,20 +1,21 @@
 import { useQuery } from "@tanstack/react-query"
 import { getCategories, getPosts } from "./actions"
+import { User } from "@/Models/User"
 
-export const useGetPosts = (accessToken: string | undefined) => {
-    const query = useQuery({
+export const useGetPosts = (user: User | null) => {
+    const accessToken = user?.accessToken
+    return useQuery({
         queryKey: ["posts", accessToken],
         enabled: !!accessToken,
         queryFn: () => getPosts(accessToken!),
     })
-    return query
 }
 
-export const useGetCategories = (accessToken: string | undefined) => {
-    const query = useQuery({
+export const useGetCategories = (user: User | null) => {
+    const accessToken = user?.accessToken
+    return useQuery({
         queryKey: ["categories", accessToken],
-        enabled: !!accessToken,
         queryFn: () => getCategories(accessToken!),
+        enabled: !!accessToken,
     })
-    return query
 }
