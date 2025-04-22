fb7c38814a0178cc03795bae69b94f98beafc987
func_Mathias <func.mathias@gmail.com>
2025-04-22 09:24:45 +0200

    Neighborhood assignment and server/client UserDTO (#116)
    Added better way of setting and leaving neighborhoods.
    Added Server and Client UserDTO's.

---
+++ webapi/DTOs/ClientUserDTO.cs
@@ -0,0 +1,33 @@
+﻿using webapi.Models;
+
+namespace webapi.DTOs;
+
+public class ClientUserDTO
+{
+    public string Name { get; set; }
+    public string? Avatar { get; set; }
+
+    public ClientUserDTO(){}
+
+    public ClientUserDTO(User user)
+    {
+        Name = user.Name;
+        Avatar = user.Avatar;
+    }
+
+    public ClientUserDTO(string name, string? avatar)
+    {
+        Name = name;
+        Avatar = avatar;
+    }
+}
+
+public class ClientUserCollectionDTO
+{
+    public IEnumerable<ClientUserDTO> Users { get; set; }
+
+    public ClientUserCollectionDTO(ICollection<User> users)
+    {
+        Users = users.Select(user => new ClientUserDTO(user));
+    }
+}
