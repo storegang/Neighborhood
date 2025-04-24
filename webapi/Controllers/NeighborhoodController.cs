85e47d6089593500c7d0514cbd5a05c0d818a2d3
func_Mathias <func.mathias@gmail.com>
2025-04-24 09:18:16 +0200

    Assign board members and related changes (#125)
    * Added calls to assign and unassign board members
    
    Also added messages to errors.
    
    * Fixed deleting neighborhood with users and fixes
    
    Can now delete a neighborhood with users, their neighborhood id's will be null and their roles will be removed.
    Added authorization for only board members to edit/delete neighborhoods, as well as needing to be a member of the neighborhood.
    Added restriction to not be able to make a new neghborhood while being a member of one.
    Added cascading delete to a few relationships to make sure things clean up as they should.
    Also added messages to errors.
    
    * Fixed get users from neighborhood not having roles
    
    * Added author/admin check to edit/delete posts
    
    Also added messages to errors.
    
    * Added author/admin check to edit/delete comments
    
    Also added messages to errors.
    
    * Added admin check to creating/editing/deleting categories
    
    Also added messages to errors.

--- webapi/Controllers/NeighborhoodController.cs
+++ webapi/Controllers/NeighborhoodController.cs
@@ -3,15 +3,19 @@ using webapi.Services;
 using webapi.Models;
 using webapi.DTOs;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using webapi.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace webapi.Controllers;
 
 [Authorize]
 [Route("api/[controller]")]
 [ApiController]
-public class NeighborhoodController(INeighborhoodService neighborhoodService) : ControllerBase
+public class NeighborhoodController(INeighborhoodService neighborhoodService, UserManager<User> userManager) : ControllerBase
 {
     private readonly INeighborhoodService _neighborhoodService = neighborhoodService;
+    private readonly UserManager<User> _userManager = userManager;
 
     // GET: api/<NeighborhoodController>
     [HttpGet]
@@ -30,7 +34,7 @@ public class NeighborhoodController(INeighborhoodService neighborhoodService) : 
         
         if (neighborhood == null)
         {
-            return NotFound();
+            return NotFound("Neighborhood not found.");
         }
 
         NeighborhoodDTO neighborhoodData = new(neighborhood);
@@ -55,11 +59,30 @@ public class NeighborhoodController(INeighborhoodService neighborhoodService) : 
             Name = neighborhoodData.Name,
             Description = neighborhoodData.Description
         };
+
+        string claimsId = User.Claims.First(c => c.Type.Equals("user_id"))?.Value ?? "";
+        User? existingUser = await _userManager.FindByIdAsync(claimsId);
+        if (existingUser == null)
+        {
+            return Unauthorized("User does not exist.");
+        }
+
+        if (!string.IsNullOrEmpty(existingUser.NeighborhoodId))
+        {
+            return BadRequest("Can not create a new neighborhood while being a member of another.");
+        }
+
+        neighborhood.Users.Add(existingUser);
         await _neighborhoodService.Create(neighborhood);
+
+        existingUser.NeighborhoodId = neighborhood.Id;
+        await _userManager.AddToRolesAsync(existingUser, [UserRoles.Tenant, UserRoles.BoardMember]);
+        await _userManager.UpdateAsync(existingUser);
 
         return CreatedAtAction(nameof(GetById), new { id = neighborhoodData.Id }, neighborhoodData);
     }
 
+    [Authorize(Roles = UserRoles.BoardMember)]
     // PUT api/<NeighborhoodController>/{id}
     [HttpPut("{id}")]
     public async Task<IActionResult> Update(string id, NeighborhoodDTO neighborhoodData)
@@ -67,7 +90,14 @@ public class NeighborhoodController(INeighborhoodService neighborhoodService) : 
         Neighborhood? existingNeighborhood = await _neighborhoodService.GetById(id);
         if (existingNeighborhood == null)
         {
-            return NotFound();
+            return NotFound("Neighborhood not found.");
+        }
+
+        string claimsId = User.Claims.First(c => c.Type.Equals("user_id"))?.Value ?? "";
+        User? claimsUser = await _userManager.FindByIdAsync(claimsId);
+        if (claimsUser == null || claimsUser.NeighborhoodId != existingNeighborhood.Id)
+        {
+            return Unauthorized("User is not a member of this neighborhood or does not exist.");
         }
 
         neighborhoodData.Id = id;
@@ -82,14 +112,28 @@ public class NeighborhoodController(INeighborhoodService neighborhoodService) : 
         return NoContent();
     }
 
+    [Authorize(Roles = UserRoles.BoardMember)]
     // DELETE api/<NeighborhoodController>/{id}
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(string id)
     {
-        Neighborhood? existingNeighborhood = await _neighborhoodService.GetById(id);
+        Neighborhood? existingNeighborhood = await _neighborhoodService.GetById(id, [query => query.Include(c => c.Users)]);
         if (existingNeighborhood == null)
         {
-            return NotFound();
+            return NotFound("Neighborhood not found.");
+        }
+
+        string claimsId = User.Claims.First(c => c.Type.Equals("user_id"))?.Value ?? "";
+        User? claimsUser = await _userManager.FindByIdAsync(claimsId);
+        if (claimsUser == null || claimsUser.NeighborhoodId != existingNeighborhood.Id)
+        {
+            return Unauthorized("User is not a member of this neighborhood or does not exist.");
+        }
+
+        foreach (User user in existingNeighborhood.Users)
+        {
+            await _userManager.RemoveFromRolesAsync(user, [UserRoles.Tenant, UserRoles.BoardMember]);
+            await _userManager.UpdateAsync(user);
         }
 
         await _neighborhoodService.Delete(id);
